a3c3b915e4c36d986e41d6a55ffe77a547259092
Kanigiri Naveen <61840214+Naveen7102@users.noreply.github.com>
2021-11-22 12:52:24 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,11 +6,11 @@ from power import power
 
 class Testpower(unittest.TestCase):
     def test_list_int(self):
-        b = 2.0
-        p = 3.0
+        b = 2
+        p = 3
         lst = [b,p]
         result=power(lst)
-        self.assertEqual(result,8.0)
+        self.assertEqual(result,8)
 
 if __name__ == '__main__':
     unittest.main()
